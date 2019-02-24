d005dd197422108568f62d733552952ea74914e3
Aypac <github@aypac.de>
2019-02-24 13:56:33 +0000

    minor fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='aypac_test_repo',
+setup(name='testrepo',
       version='0.1',
-      description='A test repo, that doesn't do anything but teach.',
+      description="A test repo, that doesn't do anything but teach.",
       url='http://github.com/aypac/PyPi_and_CI_example',
       author='Aypac',
       author_email='@aypac.de',
       license='',
-      packages=['aypac_test_repo', ],
+      packages=['testrepo', ],
       zip_safe=False)
--- tests/first_test.py
+++ tests/first_test.py
@@ -1,18 +1,20 @@
 import unittest
+import sys
+sys.path.append('..')
 import testrepo
 
 class TestAddMethods(unittest.TestCase):
 
     def test_integer(self):
-        self.assertAlmostEqual(testrepo.add(1, 2), 3)
+        self.assertAlmostEqual(testrepo.addition(1, 2), 3)
     
     def test_float(self):
-        self.assertAlmostEqual(testrepo.add(1.1, 2.2), 3.3)
+        self.assertAlmostEqual(testrepo.addition(1.1, 2.2), 3.3)
     
     def test_keyword(self):
-        self.assertAlmostEqual(testrepo.add(a=1.1, b=2.2), 3.3)
-        self.assertAlmostEqual(testrepo.add(1.1, b=2.2), 3.3)
-        self.assertAlmostEqual(testrepo.add(a=2.2), 2.2)
+        self.assertAlmostEqual(testrepo.addition(a=1.1, b=2.2), 3.3)
+        self.assertAlmostEqual(testrepo.addition(1.1, b=2.2), 3.3)
+        self.assertAlmostEqual(testrepo.addition(a=2.2), 2.2)
 
 if __name__ == '__main__':
     unittest.main()
